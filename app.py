2c5de88f270baf478a40b578801c6a388a473f5c
MPLeung <manbobo2012@gmail.com>
2020-01-25 03:28:59 +0000

    update

--- app.py
+++ app.py
@@ -44,5 +44,7 @@ if __name__ == '__main__':
     # app.run(port=5002, debug=True)
 
     # Serve the app with gevent
-    http_server = WSGIServer(('0.0.0.0', 5000), app)
-    http_server.serve_forever()
+    #http_server = WSGIServer(('0.0.0.0', 5000), app)
+    #http_server.serve_forever()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
